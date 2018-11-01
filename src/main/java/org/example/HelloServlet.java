396f3a63d8f3916358600cc6a4c07ba3570738ab
marbeen <arcilla.bumblebeen@gmail.com>
2018-11-01 13:05:25 +0800

    Initial commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+        
+        // Query Params
+        response.getWriter().println("<ul>\n" +
+        "  <li><b>First Name</b>: "
+        + request.getParameter("first_name") + "\n" +
+        "  <li><b>Last Name</b>: "
+        + request.getParameter("last_name") + "\n" +
+        "</ul>\n");
+
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+
+       // Method to handle POST method request.
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
