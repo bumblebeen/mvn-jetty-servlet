a6e4873fa9e63828759af328527c5f9f207a53d5
marbeen <arcilla.bumblebeen@gmail.com>
2018-11-01 13:40:31 +0800

    Add Servlet filters

---
+++ src/main/java/org/example/NeedsAuth.java
@@ -0,0 +1,26 @@
+package org.example;
+
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+// Extend HttpServlet class
+public class NeedsAuth extends HttpServlet {
+ 
+   // Method to handle GET method request.
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      // Set error code and reason.
+      response.sendError(404, "Need authentication!!!" );
+   }
+   
+   // Method to handle POST method request.
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      doGet(request, response);
+   }
+}
