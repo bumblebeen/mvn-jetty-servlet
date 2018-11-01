4ce430662252f4e845d8233c46d9e4d4c962eae5
marbeen <arcilla.bumblebeen@gmail.com>
2018-11-01 14:41:11 +0800

    Add Cookie handling

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +11,20 @@ public class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 4L;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        Cookie firstName = new Cookie("first_name", request.getParameter("first_name"));
+        Cookie lastName = new Cookie("last_name", request.getParameter("last_name"));
+  
+        // Set expiry date after 24 Hrs for both the cookies.
+        firstName.setMaxAge(60*60*24);
+        lastName.setMaxAge(60*60*24);
+  
+        // Add both the cookies in the response header.
+        response.addCookie( firstName );
+        response.addCookie( lastName );
+  
+
         response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+        response.setStatus(HttpServletResponse.SC_OK);        
         response.getWriter().println("<h1>Hello Servlet</h1>");
         
         // Query Params
